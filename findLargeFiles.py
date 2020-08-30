c8a213740f0dac4b3a498d4680f31f960d740f86
john doe <scottbruceburton@gmail.com>
2020-08-30 19:08:08 -0400

    TODO

---
+++ findLargeFiles.py
@@ -0,0 +1,13 @@
+#! python3
+# Searches through current directory and finds large files
+
+# TODO: min size of file - find
+
+# TODO: Transit File Structure
+
+# TODO: Make a dictionary of File/File Size
+
+# TODO: Read back found files.
+
+# TODO: Optional (ask if you would like to delete file one by one)
+
